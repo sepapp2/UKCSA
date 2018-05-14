20464796f8f1274e0bed3a1f8618ddf121b94fea
Scott Pappas <sepapp2@uky.edu>
2018-05-14 10:38:44 -0400

    Admin route guard

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,9 @@ import Router from 'vue-router'
 import Products from '@/components/Products'
 import Login from '@/components/Login'
 import SignUp from '@/components/SignUp'
+import Orders from '@/components/Orders'
 import firebase from 'firebase'
+import { db } from '../main'
 
 Vue.use(Router)
 
@@ -26,6 +28,15 @@ let router = new Router({
       }
     },
     {
+      path: '/orders',
+      name: 'Orders',
+      component: Orders,
+      meta: {
+        requiresAuth: true,
+        requiresAdmin: true
+      }
+    },
+    {
       path: '/login',
       name: 'Login',
       component: Login
@@ -41,10 +52,27 @@ let router = new Router({
 router.beforeEach((to, from, next) => {
   let currentUser = firebase.auth().currentUser
   let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
-
+  let requiresAdmin = to.matched.some(record => record.meta.requiresAdmin)
+  let isAdmin = db.collection('metadata').doc(firebase.auth().currentUser.uid).get().then(function (doc) {
+    if (doc.exists) {
+      return doc.data()
+    } else {
+      return false
+    }
+  }).catch(function (error) {
+    console.log('Error getting document:', error)
+  })
   if (requiresAuth && !currentUser) next('login')
   else if (!requiresAuth && currentUser) next()
-  else next()
+  else if (requiresAuth && requiresAdmin) {
+    isAdmin.then(function (result) {
+      if (result.admin) {
+        next()
+      } else {
+        next('products')
+      }
+    })
+  } else next()
 })
 
 export default router
